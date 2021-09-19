2dffa244c092acbcbfeb900260f3923efd2983fe
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-09-20 01:01:15 +0530

    ADDED:calculate totalwage for a month

--- src/com/bridgelabz/Empwage.java
+++ src/com/bridgelabz/Empwage.java
@@ -2,23 +2,29 @@ package com.bridgelabz;
 
 public class Empwage {
     public static void main(String[] args) {
-        int WAGE_PER_HOUR = 20 ;
         int FULL_TIME = 1;
         int PART_TIME = 2;
+        int WAGE_PER_HOUR = 20;
+        int WAGE_PER_MONTH = 20;
         int wageHrs;
         int empwage;
-        int check = (int) Math.floor(Math.random() * 10) % 3;
-        switch (check ){
-            case 1 :
-                wageHrs = 8;
-                break;
-            case 2 :
-                wageHrs = 4;
-                break;
-            default:
-                wageHrs = 0;
+        int total = 0;
+        for (int i = 0; i < WAGE_PER_MONTH; i++) {
+            int check = (int) Math.floor(Math.random() * 10) % 3;
+            switch (check) {
+                case 1:
+                    wageHrs = 8;
+                    break;
+                case 2:
+                    wageHrs = 4;
+                    break;
+                default:
+                    wageHrs = 0;
+            }
+            empwage = WAGE_PER_HOUR * wageHrs;
+            System.out.println("employee wage = " + empwage);
+            total += empwage;
         }
-         empwage = WAGE_PER_HOUR*wageHrs;
-        System.out.println("employee wage = " + empwage);
+        System.out.println("totalwage = "+total);
     }
 }
