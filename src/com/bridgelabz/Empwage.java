c26046f98eb60756e84ceac6653a2e0a0adcdf37
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-09-19 22:28:41 +0530

    ADDED:calculate daliy wage

--- src/com/bridgelabz/Empwage.java
+++ src/com/bridgelabz/Empwage.java
@@ -2,14 +2,19 @@ package com.bridgelabz;
 
 public class Empwage {
     public static void main(String[] args) {
-        int PRESENT = 0;
+        int WAGE_PER_HOUR = 20 ;
+        int FULL_DAY = 1 ;
+        int wageHrs;
+        int empwage;
         double check = Math.floor(Math.random() * 10) % 2;
-        if (check == PRESENT){
-            System.out.println("present");
+        if (check == FULL_DAY){
+           wageHrs = 8;
         }
         else
         {
-            System.out.println("absent");
+          wageHrs = 0;
         }
+         empwage = WAGE_PER_HOUR*wageHrs;
+        System.out.println("employee wage = " + empwage);
     }
 }
